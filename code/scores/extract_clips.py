c7147294a5035871e1ca9b887cde77a10d3e2af6
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-08-11 10:29:32 +0200

    extract clips

---
+++ code/scores/extract_clips.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+
+from ChildProject.projects import ChildProject
+from ChildProject.annotations import AnnotationManager
+from ChildProject.metrics import segments_to_annotation
+
+import datalad.api
+from os.path import join as opj
+from os.path import basename, exists
+import pandas as pd
+
+import numpy as np
+
+def extract_clips(parameters):
+    annotator = parameters['annotator']
+
+    project = ChildProject(parameters['path'])
+    am = AnnotationManager(project)
+    am.read()
+
+    intersection = AnnotationManager.intersection(
+        am.annotations, ['vtc', annotator]
+    )
+
+    intersection['recording_path'] = intersection['recording_filename'].apply(
+        lambda f: opj(project.path, 'recordings/raw', f)
+    )
+
+    datalad.api.get(list(intersection['recording_path'].unique()))
+
+    clips = intersection[intersection['set'] == 'vtc'][['recording_path', 'recording_filename', 'range_onset', 'range_offset']]
+    for clip in clips.to_dict(orient = 'records'):
+        signal, sr = librosa.load(clip['recording_path'], sr = 16000)
+
+        bn = splitext(basename(clip['recording_filename']))[0]
+        output = f'output/clips/{bn}_{clip['range_onset']}_{clip['range_offset']}'
+        soundfile.write(output, signal, 16000)
+
+    return clips
+    
+
+annotators = pd.read_csv('input/annotators.csv')
+annotators['path'] = annotators['corpus'].apply(lambda c: opj('input', c))
+clips = pd.concat([extract_clips(annotator) for annotator in annotators.to_dict(orient = 'records')])
+clips.to_csv('output/clips/clips.csv', index = False)
