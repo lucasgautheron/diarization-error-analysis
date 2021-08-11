e4ab6be6b52900edec784e9c26a6e039a885ea06
Lucas Gautheron <lucas.gautheron@gmail.com>
2021-08-11 10:41:11 +0200

    extract_clips fixes

--- code/scores/extract_clips.py
+++ code/scores/extract_clips.py
@@ -30,10 +30,18 @@ def extract_clips(parameters):
 
     clips = intersection[intersection['set'] == 'vtc'][['recording_path', 'recording_filename', 'range_onset', 'range_offset']]
     for clip in clips.to_dict(orient = 'records'):
-        signal, sr = librosa.load(clip['recording_path'], sr = 16000)
-
         bn = splitext(basename(clip['recording_filename']))[0]
-        output = f'output/clips/{bn}_{clip['range_onset']}_{clip['range_offset']}'
+        onset = clip['range_onset']
+        offset = clip['range_offset']
+
+        signal, sr = librosa.load(
+            clip['recording_path'],
+            sr = 16000,
+            offset = (onset-2000)/1000,
+            duration = (offset-onset+2000)/1000
+        )
+
+        output = f'output/clips/{bn}_{onset}_{offset}'
         soundfile.write(output, signal, 16000)
 
     return clips
